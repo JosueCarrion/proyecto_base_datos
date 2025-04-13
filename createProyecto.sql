27ed24376cd88b5ed90d865d29233e004a47999f
JosueCarrion <carrionjosueandres@gmail.com>
2025-04-13 19:20:48 -0400

    refoctor

--- createProyecto.sql
+++ createProyecto.sql
@@ -5,6 +5,21 @@ CREATE TABLE Impuesto (
     fecha DATE NOT NULL
 );
 
+CREATE TABLE Sucursal (
+    codigo_sucursal VARCHAR(20) NOT NULL,
+    nombre VARCHAR(100) NOT NULL,
+    direccion TEXT NOT NULL,
+    telefono VARCHAR(20) NOT NULL,
+    PRIMARY KEY (codigo_sucursal)
+);
+
+CREATE TABLE Pais (
+    codigo_pais CHAR(3),
+    costo NUMERIC(10,2) NOT NULL,
+    PRIMARY KEY (codigo_pais)
+
+);
+
 CREATE TABLE Tipo_Renta (
     codigo_renta CHAR(3) NOT NULL,
     tipo_de_telefono VARCHAR(20) NOT NULL,
@@ -12,7 +27,7 @@ CREATE TABLE Tipo_Renta (
     costo NUMERIC(10,2) NOT NULL,
     fecha DATE NOT NULL,
     cupo INTEGER NOT NULL,
-    costo_impulso_adicional NUMERIC(10,2)
+    costo_impulso_adicional NUMERIC(10,2),
     PRIMARY KEY (codigo_renta, tipo_de_telefono)
      
 );
@@ -57,49 +72,29 @@ CREATE TABLE Persona_Juridica (
     FOREIGN KEY (id_suscriptor) REFERENCES Suscriptor(id_suscriptor) ON DELETE CASCADE
 );
 
-CREATE TABLE Sucursal (
-    codigo_destino VARCHAR(20) NOT NULL,
-    codigo_sucursal VARCHAR(20) NOT NULL,
-    nombre VARCHAR(100) NOT NULL,
-    direccion TEXT NOT NULL,
-    telefono VARCHAR(20) NOT NULL,
-    PRIMARY KEY (codigo_destino, codigo_sucursal),
-    FOREIGN KEY (codigo_destino) REFERENCES Destino(codigo_destino) ON DELETE CASCADE
-);
-
-CREATE TABLE Pais (
-    codigo_destino CHAR(3) PRIMARY KEY,
-    costo NUMERIC(10,2) NOT NULL,
-    FOREIGN KEY (codigo_destino) REFERENCES Destino(codigo_destino) ON DELETE CASCADE
-);
-
-
 
 CREATE TABLE Contrato (
-    numero_telefonico VARCHAR(15) PRIMARY KEY,
+    numero_telefonico VARCHAR(15),
     titular_de_pago VARCHAR(100) NOT NULL,
     tipo_de_telefono VARCHAR(20) NOT NULL,
     direccion_de_suministro TEXT NOT NULL,
     id_suscriptor INT NOT NULL,
     codigo_renta CHAR(3) NOT NULL,
     codigo_sucursal VARCHAR(20),
+    PRIMARY KEY(codigo_sucursal, numero_telefonico),
     FOREIGN KEY (id_suscriptor) REFERENCES Suscriptor(id_suscriptor) ON DELETE CASCADE,
-    FOREIGN KEY (codigo_renta) REFERENCES Tipo_Renta(codigo_renta) ON DELETE CASCADE
+    FOREIGN KEY (codigo_renta, tipo_de_telefono) REFERENCES Tipo_Renta(codigo_renta, tipo_de_telefono) ON DELETE CASCADE,
+    FOREIGN KEY (codigo_sucursal) REFERENCES Sucursal(codigo_sucursal)
 );
 
-CREATE TABLE Cheque (
-    cod_cheque VARCHAR(20) PRIMARY KEY,
-    numero_cheque VARCHAR(20) NOT NULL,
-    operador_telefonico VARCHAR(50) NOT NULL,
-    banco VARCHAR(50) NOT NULL,
-    clave VARCHAR(20) NOT NULL
-);
+
 
 CREATE TABLE pagado (
     codigo_pago_oficina INT PRIMARY KEY ,
+    codigo_sucursal VARCHAR(20),
     numero_telefonico VARCHAR(20) NOT NULL,
     tipo_de_pago VARCHAR(20) NOT NULL CHECK (tipo_de_pago IN ('Automático', 'Por Oficina')),
-    FOREIGN KEY (numero_telefonico) REFERENCES Contrato(numero_telefonico) ON DELETE CASCADE
+    FOREIGN KEY (codigo_sucursal, numero_telefonico) REFERENCES Contrato(codigo_sucursal, numero_telefonico) ON DELETE CASCADE
 
 );
 
@@ -108,24 +103,32 @@ CREATE TABLE Pago (
     numero_de_tarjeta VARCHAR(20) NULL,
     fecha DATE NOT NULL,
     monto NUMERIC(10,2) NOT NULL,
-    codigo_cheque VARCHAR(20) NULL,
-    FOREIGN KEY (codigo_cheque) REFERENCES Cheque(cod_cheque) ON DELETE SET NULL,
      FOREIGN KEY (codigo_pago_oficina) REFERENCES pagado(codigo_pago_oficina) ON DELETE CASCADE
 );
 
+CREATE TABLE Cheque (
+    cod_cheque VARCHAR(20) PRIMARY KEY,
+    numero_cheque VARCHAR(20) NOT NULL,
+    operador_telefonico VARCHAR(50) NOT NULL,
+    banco VARCHAR(50) NOT NULL,
+    clave VARCHAR(20) NOT NULL
+)INHERITS (Pago);
+
 CREATE TABLE Pago_Por_Oficina (
+    codigo_sucursal VARCHAR(20),
     numero_telefonico VARCHAR(20) NOT NULL,
     codigo_pago_oficina INT PRIMARY KEY,
-    FOREIGN KEY (numero_telefonico) REFERENCES Contrato(numero_telefonico) ON DELETE CASCADE,
+    FOREIGN KEY (codigo_sucursal, numero_telefonico) REFERENCES Contrato(codigo_sucursal, numero_telefonico) ON DELETE CASCADE,
     FOREIGN KEY (codigo_pago_oficina) REFERENCES pagado(codigo_pago_oficina) ON DELETE CASCADE
 );
 
 CREATE TABLE Pago_Automatico (
     codigo_pago_automatico INT NOT NULL,
+    codigo_sucursal VARCHAR(20),
     numero_telefonico VARCHAR(20) NOT NULL,
     numero_de_tarjeta VARCHAR(20) NOT NULL,
-    PRIMARY KEY (codigo_pago_automatico, numero_telefonico, numero_de_tarjeta),
-    FOREIGN KEY (numero_telefonico) REFERENCES Contrato(numero_telefonico) ON DELETE CASCADE,
+    PRIMARY KEY (codigo_pago_automatico, codigo_sucursal, numero_telefonico, numero_de_tarjeta),
+    FOREIGN KEY (codigo_sucursal,numero_telefonico) REFERENCES Contrato(codigo_sucursal, numero_telefonico) ON DELETE CASCADE,
     FOREIGN KEY (codigo_pago_automatico) REFERENCES pagado(codigo_pago_oficina) ON DELETE CASCADE
 );
 
@@ -133,6 +136,7 @@ CREATE TABLE Pago_Automatico (
 
 CREATE TABLE Llamada (
     id_llamada SERIAL,
+    codigo_sucursal VARCHAR(20),
     numero_destino VARCHAR(20) NULL,
     operador VARCHAR(3) NOT NULL,
     minutos INTEGER NOT NULL,
@@ -141,20 +145,26 @@ CREATE TABLE Llamada (
     hora TIME NOT NULL,
     numero_telefonico VARCHAR(20) NOT NULL,
     codigo_destino VARCHAR(20) NULL,
-    compania_celular CHAR(3) NULL,
     PRIMARY KEY (id_llamada),
-    FOREIGN KEY (numero_telefonico) REFERENCES Contrato(numero_telefonico) ON DELETE CASCADE,
-    FOREIGN KEY (codigo_destino) REFERENCES Destino(codigo_destino) ON DELETE CASCADE,
-    FOREIGN KEY (compania_celular) REFERENCES Compania_Celular(codigo_compania) ON DELETE CASCADE
+    FOREIGN KEY (codigo_sucursal, numero_telefonico) REFERENCES Contrato(codigo_sucursal, numero_telefonico) ON DELETE CASCADE,
+    FOREIGN KEY (codigo_destino) REFERENCES Destino(codigo_destino) ON DELETE CASCADE
 );
 
+CREATE TABLE Celular (
+    compania_celular CHAR(3) NOT NULL,
+    PRIMARY KEY (id_llamada, compania_celular),
+    FOREIGN KEY (compania_celular) REFERENCES Compania_Celular(codigo_compania) ON DELETE CASCADE
+) INHERITS (Llamada);
+
+
 CREATE TABLE Tiene_servicio (
+    codigo_sucursal VARCHAR(20) NOT NULL,
     numero_telefonico VARCHAR(20) NOT NULL,
     codigo_servicio VARCHAR(20) NOT NULL,
     fecha_inicio DATE NOT NULL,
     fecha_fin DATE,
-    PRIMARY KEY (numero_telefonico, codigo_servicio),
-    FOREIGN KEY (numero_telefonico) REFERENCES Contrato(numero_telefonico) ON DELETE CASCADE,
+    PRIMARY KEY (codigo_sucursal, numero_telefonico, codigo_servicio),
+    FOREIGN KEY (codigo_sucursal, numero_telefonico) REFERENCES Contrato(codigo_sucursal, numero_telefonico) ON DELETE CASCADE,
     FOREIGN KEY (codigo_servicio) REFERENCES Servicio(codigo_servicio) ON DELETE CASCADE,
     CHECK (fecha_fin IS NULL OR fecha_fin >= fecha_inicio)
 );
@@ -165,6 +175,6 @@ CREATE TABLE Cuesta (
     monto NUMERIC(10,2) NOT NULL,
     fecha DATE NOT NULL,
     PRIMARY KEY (origen, destino),
-    FOREIGN KEY (origen) REFERENCES Destino(codigo_destino) ON DELETE CASCADE,
-    FOREIGN KEY (destino) REFERENCES Destino(codigo_destino) ON DELETE CASCADE
+    FOREIGN KEY (origen) REFERENCES Sucursal(codigo_sucursal) ON DELETE CASCADE,
+    FOREIGN KEY (destino) REFERENCES Sucursal(codigo_sucursal) ON DELETE CASCADE
 );
--- insertProyecto.sql
+++ insertProyecto.sql
@@ -1,9 +1,16 @@
+-- Tarifas internacionales
+INSERT INTO Pais (codigo_pais, costo) VALUES
+    ('VEN', 0),
+    ('ESP', 9.80),
+    ('ITA', 10.00),
+    ('EUA', 7.60);
+
 -- Destinos
 INSERT INTO Destino (abreviatura, codigo_destino, nombre) VALUES
-    ('CAR', '0212', 'Caracas'),
-    ('LTQ','0232', 'Los Teques'),
-    ('CAZ','0233', 'Carayaca'),
-    ('AVE','0234', 'Los Velásquez'),
+    ('VEN', '0212', 'Caracas'),
+    ('VEN','0232', 'Los Teques'),
+    ('VEN','0233', 'Carayaca'),
+    ('VEN','0234', 'Los Velásquez'),
     ('ESP','034', 'España'),
     ('ITA','039', 'Italia'),
     ('EUA','001', 'Estados Unidos de América');
@@ -17,7 +24,7 @@ INSERT INTO Impuesto (codigo_de_impuesto, descripcion, monto, fecha) VALUES
 
 -- Tipo de Renta
 INSERT INTO Tipo_Renta (codigo_renta, descripcion, tipo_de_telefono,  costo, fecha, cupo, costo_impulso_adicional) VALUES
-    ('BAS', 'Básica', 'Residencial' 194.00, '2020-08-01', 50, 5.17),
+    ('BAS', 'Básica', 'Residencial', 194.00, '2020-08-01', 50, 5.17),
     ('INT', 'Intermedia','Residencial', 304.00, '2020-08-01', 90, 3.69),
     ('REN', 'Rendidora Residencial','Residencial', 555.00, '2020-08-01', 130, 3.19),
     ('UNI', 'Única', 'Comercial',  800.00, '2020-08-01', 0, 5.75);
@@ -32,18 +39,14 @@ INSERT INTO Compania_Celular (codigo_compania, monto, fecha, descripcion) VALUES
     ('MOV', 14.60, '2020-07-01', 'Movilcel'),
     ('TEL', 15.00, '2020-09-01', 'Telenet');
 
--- Tarifas internacionales
-INSERT INTO Pais (codigo_destino, costo) VALUES
-    ('034', 9.80),
-    ('039', 10.00),
-    ('001', 7.60);
+
 
 -- Tarifas Nacionales
-INSERT INTO Sucursal (codigo_destino, codigo_sucursal, nombre, direccion, telefono) VALUES
-    ('0212', 'SUC01', 'Sucursal Caracas', 'Av. Principal #123, Caracas', '0212-5551234'),
-    ('0232', 'SUC02', 'Sucursal Los Teques', 'Calle Norte #456, Los Teques', '0232-5555678'),
-    ('0233', 'SUC03', 'Sucursal Carayaca', 'Av. Principal #789, Carayaca', '0233-5559012'),
-    ('0234', 'SUC04', 'Sucursal Los Velásquez', 'Calle Sur #101, Los Velásquez', '0234-5553456');
+INSERT INTO Sucursal (codigo_sucursal, nombre, direccion, telefono) VALUES
+    ('0212', 'Sucursal Caracas', 'Av. Principal #123, Caracas', '0212-5551234'),
+    ('0232', 'Sucursal Los Teques', 'Calle Norte #456, Los Teques', '0232-5555678'),
+    ('0233', 'Sucursal Carayaca', 'Av. Principal #789, Carayaca', '0233-5559012'),
+    ('0234', 'Sucursal Los Velásquez', 'Calle Sur #101, Los Velásquez', '0234-5553456');
 
 
 -- ************************************************************
@@ -71,44 +74,44 @@ INSERT INTO Persona_Juridica (rif, razon_social, id_suscriptor) VALUES
 
 
 -- Contratos
-INSERT INTO Contrato (numero_telefonico, titular_de_pago, tipo_de_telefono, direccion_de_suministro, id_suscriptor, codigo_renta) VALUES
-    ('0234-2576389', 'José Eladio Martínez Díaz', 'Residencial', 'Urb. Carrizales, calle 14, Nro. 153', 1, 'BAS'),
-    ('0212-6622590', 'Milexa Deris Nieto', 'Residencial', 'Urb. La Colina, Edif. Girasol, Apto. 22', 2, 'INT'),
-    ('0212-4844356', 'Vicenta Rivero de Bracho', 'Residencial', 'Urb. La Cascada, Edif. Némesis, Apto. 5-A', 3, 'REN'),
-    ('0212-4435560', 'Sistemas J.J. y Asociados', 'Comercial', 'C.C. Centro Plaza, nivel 3, local 35', 4, 'UNI');
+INSERT INTO Contrato (codigo_sucursal, numero_telefonico, titular_de_pago, tipo_de_telefono, direccion_de_suministro, id_suscriptor, codigo_renta) VALUES
+    ('0234', '0234-2576389', 'José Eladio Martínez Díaz', 'Residencial', 'Urb. Carrizales, calle 14, Nro. 153', 1, 'BAS'),
+    ('0212', '0212-6622590', 'Milexa Deris Nieto', 'Residencial', 'Urb. La Colina, Edif. Girasol, Apto. 22', 2, 'INT'),
+    ('0212', '0212-4844356', 'Vicenta Rivero de Bracho', 'Residencial', 'Urb. La Cascada, Edif. Némesis, Apto. 5-A', 3, 'REN'),
+    ('0212', '0212-4435560', 'Sistemas J.J. y Asociados', 'Comercial', 'C.C. Centro Plaza, nivel 3, local 35', 4, 'UNI');
 
-INSERT INTO Cheque (cod_cheque, numero_cheque, operador_telefonico, banco, clave) VALUES
-    ('CHQ001', '3445645646', '43', 'Banco de Venezuela', '3443');
-
-
-
-INSERT INTO pagado (codigo_pago_oficina, numero_telefonico, tipo_de_pago) VALUES
-    (1,'0234-2576389', 'Por Oficina'),
-    (2,'0212-6622590', 'Por Oficina'),
-    (3,'0212-4844356', 'Por Oficina'),
-    (4,'0212-4435560', 'Automático');
-
-    INSERT INTO Pago (codigo_pago_oficina, numero_de_tarjeta, fecha, monto, codigo_cheque) VALUES
-    (1, NULL, '2020-10-04', 576.80, NULL),
-    (2, NULL, '2020-10-27', 257.50, 'CHQ001'),
-    (3, NULL, '2020-10-28', 30.00, NULL),
-    (4, '4554-333333333333', '2020-10-01', 2400.00, NULL);
-
-
-INSERT INTO Pago_Por_Oficina (numero_telefonico, codigo_pago_oficina) VALUES
-    ('0234-2576389', 1),
-    ('0212-6622590', 2),
-    ('0212-4844356', 3);
-
-INSERT INTO Pago_Automatico (codigo_pago_automatico, numero_telefonico, numero_de_tarjeta) VALUES
-    (4, '0212-4435560', '4554-333333333333');
+INSERT INTO Cheque (codigo_pago_oficina, numero_de_tarjeta, fecha, monto, cod_cheque, numero_cheque, operador_telefonico, banco, clave) VALUES
+    (2, NULL, '2020-10-27', 257.50, 'CHQ001', '3445645646', '43', 'Banco de Venezuela', '3443');
 
 
 
-INSERT INTO Tiene_servicio (numero_telefonico, codigo_servicio, fecha_inicio, fecha_fin) VALUES
-    ('0212-6622590', 'STA', '2020-10-01', NULL),
-    ('0212-6622590', 'SNP', '2020-10-01', NULL),
-    ('0212-4844356', 'STA', '2020-10-01', NULL);
+INSERT INTO pagado (codigo_pago_oficina, codigo_sucursal,numero_telefonico, tipo_de_pago) VALUES
+    (1, '0234', '0234-2576389', 'Por Oficina'),
+    (2, '0212', '0212-6622590', 'Por Oficina'),
+    (3, '0212', '0212-4844356', 'Por Oficina'),
+    (4, '0212', '0212-4435560', 'Automático');
+
+    INSERT INTO Pago (codigo_pago_oficina, numero_de_tarjeta, fecha, monto) VALUES
+    (1, NULL, '2020-10-04', 576.80),
+    (2, NULL, '2020-10-27', 257.50),
+    (3, NULL, '2020-10-28', 30.00),
+    (4, '4554-333333333333', '2020-10-01', 2400.00);
+
+
+INSERT INTO Pago_Por_Oficina (codigo_sucursal, numero_telefonico, codigo_pago_oficina) VALUES
+    ('0234', '0234-2576389', 1),
+    ('0212', '0212-6622590', 2),
+    ('0212', '0212-4844356', 3);
+
+INSERT INTO Pago_Automatico (codigo_pago_automatico, codigo_sucursal,numero_telefonico, numero_de_tarjeta) VALUES
+    (4, '0212', '0212-4435560', '4554-333333333333');
+
+
+
+INSERT INTO Tiene_servicio (codigo_sucursal, numero_telefonico, codigo_servicio, fecha_inicio, fecha_fin) VALUES
+    ('0212', '0212-6622590', 'STA', '2020-10-01', NULL),
+    ('0212', '0212-6622590', 'SNP', '2020-10-01', NULL),
+    ('0212', '0212-4844356', 'STA', '2020-10-01', NULL);
 
 INSERT INTO Cuesta (origen, destino, monto, fecha) VALUES
     ('0234', '0212', 5.80, '2020-08-01'),
@@ -118,52 +121,60 @@ INSERT INTO Cuesta (origen, destino, monto, fecha) VALUES
     ('0212', '0233', 3.60, '2020-08-01'),
     ('0233', '0232', 2.50, '2020-08-01');
 
-INSERT INTO Llamada (numero_destino, operador, minutos, segundos, fecha, hora, numero_telefonico, codigo_destino, compania_celular) VALUES
-    ('0212-9063999', 'CAR', 0, 41, '2020-10-05', '21:35:09', '0234-2576389', '0212', null),
-    ('0232-2344441', 'LTQ', 1, 02, '2020-10-15', '12:42:09', '0234-2576389', '0232', null),
-    ('0233-3455511', 'CAZ', 3, 39, '2020-10-26', '08:06:29', '0234-2576389', '0233', null),
-    ('0212-9773044', 'CAR', 1, 16, '2020-10-17', '11:28:39', '0234-2576389', '0212', null),
-    ('0340034439007', 'ESP', 6, 00, '2020-10-20', '06:20:06', '0234-2576389', '034', null),
-    ('0019082231083', 'EUA', 2, 00, '2020-10-24', '22:07:00', '0234-2576389', '001', null);
+INSERT INTO Llamada (numero_destino, operador, minutos, segundos, fecha, hora, codigo_sucursal, numero_telefonico, codigo_destino) VALUES
+    ('0212-9063999', 'CAR', 0, 41, '2020-10-05', '21:35:09', '0234', '0234-2576389', '0212'),
+    ('0232-2344441', 'LTQ', 1, 02, '2020-10-15', '12:42:09', '0234', '0234-2576389', '0232'),
+    ('0233-3455511', 'CAZ', 3, 39, '2020-10-26', '08:06:29', '0234', '0234-2576389', '0233'),
+    ('0212-9773044', 'CAR', 1, 16, '2020-10-17', '11:28:39', '0234', '0234-2576389', '0212'),
+    ('0340034439007', 'ESP', 6, 00, '2020-10-20', '06:20:06', '0234', '0234-2576389', '034'),
+    ('0019082231083', 'EUA', 2, 00, '2020-10-24', '22:07:00', '0234', '0234-2576389', '001');
     
-INSERT INTO Llamada (numero_destino, operador, minutos, segundos, fecha, hora, numero_telefonico, codigo_destino, compania_celular) VALUES
-    ('0416-7345559', 'CAR', 9, 38, '2020-10-30', '20:06:29', '0212-6622590', '0212', 'MOV'),
-    ('0414-0319889', 'CAR', 6, 43, '2020-10-20', '16:20:01', '0212-6622590', '0212', 'TEL'),
-    ('0232-6334942', 'LTQ', 1, 42, '2020-10-20', '12:42:09', '0212-6622590', '0232', null);
+INSERT INTO Llamada (numero_destino, operador, minutos, segundos, fecha, hora, codigo_sucursal, numero_telefonico, codigo_destino) VALUES
+    ('0232-6334942', 'LTQ', 1, 42, '2020-10-20', '12:42:09', '0212', '0212-6622590', '0232');
+
+INSERT INTO Celular (numero_destino, operador, minutos, segundos, fecha, hora, codigo_sucursal, numero_telefonico, codigo_destino, compania_celular) VALUES
+    ('0416-7345559', 'CAR', 9, 38, '2020-10-30', '20:06:29', '0212', '0212-6622590', '0212', 'MOV'),
+    ('0414-0319889', 'CAR', 6, 43, '2020-10-20', '16:20:01', '0212', '0212-6622590', '0212', 'TEL');
 
 
 
-INSERT INTO Llamada (numero_destino, operador, minutos, segundos, fecha, hora, numero_telefonico, codigo_destino, compania_celular) VALUES
-    ('0416-5312345', 'CAR', 9, 38, '2020-10-20', '20:06:29', '0212-4844356', '0212', 'MOV'),
-    ('0416-5312345', 'CAR', 11, 38, '2020-10-20', '20:20:29', '0212-4844356', '0212', 'MOV'),
-    ('0414-2837200', 'CAR', 1, 08, '2020-10-20', '20:17:30', '0212-4844356', '0212', 'TEL'),
-    ('0232-6334942', 'LTQ', 11, 23, '2020-10-16', '12:42:09', '0212-4844356', '0232', null),
-    ('0232-6334942', 'LTQ', 31, 23, '2020-10-17', '02:40:01', '0212-4844356', '0232', null),
-    ('0232-6334942', 'LTQ', 90, 23, '2020-10-17', '14:00:06', '0212-4844356', '0232', null),
-    ('0340014490207', 'ESP', 26, 00, '2020-10-20', '16:20:06', '0212-4844356', '034', null),
-    ('0340014490207', 'ESP', 47, 00, '2020-10-22', '06:00:36', '0212-4844356', '034', null),
-    ('0390312407563', 'ITA', 7, 00, '2020-10-22', '22:00:36', '0212-4844356', '039', null),
-    ('0019031247563', 'EUA', 21, 00, '2020-10-17', '00:00:54', '0212-4844356', '001', null);
+INSERT INTO Llamada (numero_destino, operador, minutos, segundos, fecha, hora, codigo_sucursal, numero_telefonico, codigo_destino) VALUES
+    ('0232-6334942', 'LTQ', 11, 23, '2020-10-16', '12:42:09', '0212', '0212-4844356', '0232'),
+    ('0232-6334942', 'LTQ', 31, 23, '2020-10-17', '02:40:01', '0212', '0212-4844356', '0232'),
+    ('0232-6334942', 'LTQ', 90, 23, '2020-10-17', '14:00:06', '0212', '0212-4844356', '0232'),
+    ('0340014490207', 'ESP', 26, 00, '2020-10-20', '16:20:06', '0212', '0212-4844356', '034'),
+    ('0340014490207', 'ESP', 47, 00, '2020-10-22', '06:00:36', '0212', '0212-4844356', '034'),
+    ('0390312407563', 'ITA', 7, 00, '2020-10-22', '22:00:36', '0212', '0212-4844356', '039'),
+    ('0019031247563', 'EUA', 21, 00, '2020-10-17', '00:00:54', '0212', '0212-4844356', '001');
+
+INSERT INTO Celular (numero_destino, operador, minutos, segundos, fecha, hora, codigo_sucursal, numero_telefonico, codigo_destino, compania_celular) VALUES
+    ('0416-5312345', 'CAR', 9, 38, '2020-10-20', '20:06:29', '0212', '0212-4844356', '0212', 'MOV'),
+    ('0416-5312345', 'CAR', 11, 38, '2020-10-20', '20:20:29', '0212', '0212-4844356', '0212', 'MOV'),
+    ('0414-2837200', 'CAR', 1, 08, '2020-10-20', '20:17:30', '0212', '0212-4844356', '0212', 'TEL');
 
 
+INSERT INTO Celular (numero_destino, operador, minutos, segundos, fecha, hora, codigo_sucursal, numero_telefonico, codigo_destino, compania_celular) VALUES
+    ('0416-5312345', 'CAR', 9, 38, '2020-10-20', '20:06:29', '0212', '0212-4844356', '0212', 'MOV'),
+    ('0416-5312345', 'CAR', 11, 38, '2020-10-20', '20:20:29', '0212', '0212-4844356', '0212', 'MOV'),
+    ('0414-2837200', 'CAR', 1, 08, '2020-10-20', '20:17:30', '0212', '0212-4844356', '0212', 'TEL'),
+    ('0416-3312336', 'CAR', 19, 38, '2020-10-12', '20:06:29', '0212', '0212-4435560', '0212', 'MOV'),
+    ('0416-8312347', 'CAR', 1, 38, '2020-10-14', '20:40:29', '0212', '0212-4435560', '0212', 'MOV'),
+    ('0414-2837201', 'CAR', 11, 08, '2020-10-18', '20:17:30', '0212', '0212-4435560', '0212', 'TEL'),
+    ('0414-2833402', 'LTQ', 13, 08, '2020-10-19', '10:17:30', '0212', '0212-4435560', '0232', 'TEL');
 
-INSERT INTO Llamada (numero_destino, operador, minutos, segundos, fecha, hora, numero_telefonico, codigo_destino, compania_celular) VALUES
-    ('0416-3312336', 'CAR', 19, 38, '2020-10-12', '20:06:29', '0212-4435560', '0212', 'MOV'),
-    ('0416-8312347', 'CAR', 1, 38, '2020-10-14', '20:40:29', '0212-4435560', '0212', 'MOV'),
-    ('0414-2837201', 'CAR', 11, 08, '2020-10-18', '20:17:30', '0212-4435560', '0212', 'TEL'),
-    ('0414-2833402', 'LTQ', 13, 08, '2020-10-19', '10:17:30', '0212-4435560', '0232', 'TEL'),
-    ('0390312199098', 'ITA', 12, 00, '2020-10-15', '12:00:36', '0212-4435560', '039', null),
-    ('0390312199098', 'ITA', 12, 00, '2020-10-16', '12:00:32', '0212-4435560', '039', null),
-    ('0390312199098', 'ITA', 12, 00, '2020-10-17', '21:00:33', '0212-4435560', '039', null),
-    ('0390312199098', 'ITA', 12, 00, '2020-10-18', '13:00:55', '0212-4435560', '039', null),
-    ('0390312199098', 'ITA', 12, 00, '2020-10-19', '14:00:13', '0212-4435560', '039', null),
-    ('0390312199098', 'ITA', 12, 00, '2020-10-20', '22:00:24', '0212-4435560', '039', null),
-    ('0390312199098', 'ITA', 12, 00, '2020-10-22', '22:00:36', '0212-4435560', '039', null),
-    ('0340014480072', 'ESP', 16, 00, '2020-10-21', '16:20:06', '0212-4435560', '034', null),
-    ('0340014341992', 'ESP', 17, 00, '2020-10-22', '06:00:36', '0212-4435560', '034', null),
-    ('0019031247563', 'EUA', 21, 00, '2020-10-17', '00:00:54', '0212-4435560', '001', null),
-    ('0019031247563', 'EUA', 51, 00, '2020-10-18', '10:00:43', '0212-4435560', '001', null),
-    ('0019031247563', 'EUA', 11, 00, '2020-10-18', '11:14:33', '0212-4435560', '001', null),
-    ('0019031247563', 'EUA', 1, 00, '2020-10-19', '11:00:41', '0212-4435560', '001', null);
+INSERT INTO Llamada (numero_destino, operador, minutos, segundos, fecha, hora,codigo_sucursal, numero_telefonico, codigo_destino) VALUES
+    ('0390312199098', 'ITA', 12, 00, '2020-10-15', '12:00:36', '0212', '0212-4435560', '039'),
+    ('0390312199098', 'ITA', 12, 00, '2020-10-16', '12:00:32', '0212', '0212-4435560', '039'),
+    ('0390312199098', 'ITA', 12, 00, '2020-10-17', '21:00:33', '0212', '0212-4435560', '039'),
+    ('0390312199098', 'ITA', 12, 00, '2020-10-18', '13:00:55', '0212', '0212-4435560', '039'),
+    ('0390312199098', 'ITA', 12, 00, '2020-10-19', '14:00:13', '0212', '0212-4435560', '039'),
+    ('0390312199098', 'ITA', 12, 00, '2020-10-20', '22:00:24', '0212', '0212-4435560', '039'),
+    ('0390312199098', 'ITA', 12, 00, '2020-10-22', '22:00:36', '0212', '0212-4435560', '039'),
+    ('0340014480072', 'ESP', 16, 00, '2020-10-21', '16:20:06', '0212', '0212-4435560', '034'),
+    ('0340014341992', 'ESP', 17, 00, '2020-10-22', '06:00:36', '0212', '0212-4435560', '034'),
+    ('0019031247563', 'EUA', 21, 00, '2020-10-17', '00:00:54', '0212', '0212-4435560', '001'),
+    ('0019031247563', 'EUA', 51, 00, '2020-10-18', '10:00:43', '0212', '0212-4435560', '001'),
+    ('0019031247563', 'EUA', 11, 00, '2020-10-18', '11:14:33', '0212', '0212-4435560', '001'),
+    ('0019031247563', 'EUA', 1, 00, '2020-10-19', '11:00:41', '0212', '0212-4435560', '001');
 
     
